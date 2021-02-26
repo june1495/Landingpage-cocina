f1df35405206b549153aa8906ace524c59ecabd4
june1495 <71852475+june1495@users.noreply.github.com>
2021-02-25 23:22:27 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -14,13 +14,38 @@ function toggleMenu() {
 // Scroll Reveal
 window.sr = ScrollReveal();
 
-sr.reveal('.navbar', {
+sr.reveal('.awelo', {
     duration: 2000,
     origin: 'bottom',
-    distance:'-100px'
+    
 });
 sr.reveal('.content', {
     duration: 2000,
     origin: 'bottom',
     distance:'-100px'
 });
+//LISTEN FOR A SUBMIT
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+function submitForm(e) {
+    e.preventDefault();
+
+ // GET INPUT VALUES
+let name = document.querySelector(".name").value;
+let email = document.querySelector(".email").value;
+let message = document.querySelector(".message").value;
+document.querySelector(".contact-form").reset();
+sendEmail(name, email, message);
+}
+
+// SEND EMAIL
+function sendEmail(name, email, message) {
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "francomelgar4@gmail.com",
+        Password : "nivvhtjtnjbmhcfa",
+        To : 'francomelgar4@gmail.com',
+        From : "francomelgar4@gmail.com",
+        Subject : `${name} sent you a message`,
+        Body : `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`,
+    }).then((message) => alert("mail sent successfully"))
+}
